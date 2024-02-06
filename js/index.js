c19cf35d1b401a0af5c275fcec9bfdd68e2dd2dd
ybeast313 <bopsdlops@gmail.com>
2024-02-06 10:55:19 +0545

    animation

--- js/index.js
+++ js/index.js
@@ -1,29 +1,21 @@
-// console.log("Hello world!")
-// console.warn("Pratik")
-// console.error("pratik")
-
-// let clz = "abc";
-// console.log(clz)
-// clz = "xyz"
-// console.log(clz)
-
-let arrayone = [
-    { title: "WORKING TIME: " },
-    { title: "FIND US:  " },
-]
-let datatwo = '';
+let headerTitle = [
+    { title: "WORKING TIME: ", content: "Monday - Friday: 08 AM - 06 PM" },
+    { title: "FIND US:  ", content: "40 Baria Street 133/2, New York City, US" },
+];
 
 
-arrayone.map((value, index) => {
-    datatwo += `
-    <div class = "work">
-    <div class = "icon"></div>
-    <div>
-    <p class="info_title">${value.title
-        }</p >
-<div>9:00 - 5:00</div>
-    </div > `
-})
+let data = '';
 
-let info_data = document.getElementById("info");                //assigns the div with id "info" to variable info_data 
-info_data.innerHTML = datatwo;
+headerTitle.map((value, index) => {
+    data += `
+    <div class="work">
+        <div class="icon"></div>
+        <div>
+            <p class="info_title">${value.title}</p>
+            <div class = "info_content">${value.content}</div>
+        </div>
+    </div>`;
+});
+
+let info_data = document.getElementById("info");
+info_data.innerHTML = data;
--- push.cmd
+++
@@ -1,5 +0,0 @@
-git init
-git add .
-git commit -am "message"
-git remote add origin "url"
-git  push -u origin master
